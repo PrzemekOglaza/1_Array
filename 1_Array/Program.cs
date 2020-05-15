8d2b4ba1c92eb80b60a979079f50c20f298d2ff3
PrzemekO <przemyslaw.oglaza@outlook.com>
2020-05-15 18:12:22 +0200

    Tablica - wyszukanie w tabicy, modyfikacja w tablicy

--- 1_Array/Program.cs
+++ 1_Array/Program.cs
@@ -6,7 +6,46 @@ namespace _1_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] monthsOfYear =
+            {              
+                "January",
+                "Febru",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+            Console.WriteLine("Przed zmianą: ");
+
+            foreach (var month in monthsOfYear)
+
+                Console.WriteLine(month);
+
+
+            monthsOfYear[1] = "February";
+
+            Console.WriteLine("\nPo zmnianie: ");
+
+            foreach (var month in monthsOfYear)
+
+                Console.WriteLine(month);
+            /*
+        }
+            
+            Console.WriteLine("Wybierz miesiąc który chcesz wyświetlić ?");
+            Console.WriteLine("January = 1, February = 2, itd . : ");
+
+            int month = int.Parse(Console.ReadLine());
+            string chosenMonth = monthsOfYear[month - 1];
+
+            Console.WriteLine($"Wybrany miesiąc to {chosenMonth}");
+            */
         }
     }
 }
